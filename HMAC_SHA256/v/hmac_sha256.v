e8ef63cedd5dcb65afb5e0b5ed02ba1bd5fa484f
Davis Henry Sauer <dsauer@linux-lab-049.ece.uw.edu>
2024-04-17 21:34:56 +0000

    updated project skeleton

--- HMAC_SHA256/v/hmac_sha256.v
+++ HMAC_SHA256/v/hmac_sha256.v
@@ -1,60 +1,28 @@
 /**
-* Conway's Game of Life Cell
-*
-* data_i[7:0] is status of 8 neighbor cells
-* data_o is status this cell
-* 1: alive, 0: death
-*
-* when en_i==1:
-*   simulate the cell transition with 8 given neighors
-* else when update_i==1:
-*   update the cell status to update_val_i
-* else:
-*   cell status remains unchanged
+* HMAC using SHA256 Encryption
 **/
 
-module bsg_cgol_cell (
-    input clk_i
-
-    ,input en_i          
-    ,input [7:0] data_i
-
-    ,input update_i     
-    ,input update_val_i
-
-    ,output logic data_o
+module hmac_sha256 (
+     input clk_i
+    ,input rst_i 
+        
+    ,input [255:0] prf_i
+    ,input [255:0] salt_i
+    
+    ,output logic [255:0] prf_o
+    
+    ,output prf_v_o
+    ,input prf_r_i
   );
   
-/*
-    logic [3:0] num_ones;
-    bsg_popcount #(.width_p(8)) count_ones (
-        .i( data_i ),
-        .o( num_ones )
-    );
-    */
-	logic [3:0] num_ones;
-	logic [1:0] psum_1, psum_2, psum_3, psum_4;
-	logic [2:0] psum_5, psum_6;
+    logic in_valid, inready, out_valid, out_ready;
+    logic [255:0] out;
+    logic [511:0] in;
+    sha256 hasher (.clk_i, .rst_i, .in_valid, .in, .in_ready, .out_valid, .out, .out_ready);
 
-	always @(*) begin
-    	psum_1 = data_i[7] + data_i[6];
-    	psum_2 = data_i[5] + data_i[4];
-    	psum_3 = data_i[3] + data_i[2];
-    	psum_4 = data_i[1] + data_i[0];
-    	psum_5 = psum_1 + psum_2;
-    	psum_6 = psum_3 + psum_4;
-    	num_ones = psum_5 + psum_6;
-	end
-
+    
     always @(posedge clk_i) begin
-        if(en_i) begin  // calc new value
-	    data_o <= (data_o | num_ones[0]) & num_ones[1] & !num_ones[2]; // logical equivalent of 2 or 3
-	    end else if(update_i) begin
-            //cell_r <= update_val_i;
-            data_o <= update_val_i;  // take given value
-        end  else begin
-            data_o <= data_o;  // remain unchanged
-        end 
+      
     end
 endmodule
 
--- PBKDF2/v/pbkdf2.v
+++ PBKDF2/v/pbkdf2.v
@@ -2,14 +2,16 @@
 * PBKDF2 key generatoring algorithm 
 **/
 
-module PBKDF2_chunk(
-     input clk_i, rst_i
+module pbkdf2_chunk(
+     input clk_i
+    ,input rst_i
     ,input [31:0] iters_i
-    ,input [255:0] pass_i, salt_i
+    ,input [255:0] pass_i
+    ,input [255:0] salt_i
 
     ,input ready_i     
-
     ,output logic valid_o
+
     ,output logic [255:0] hash_o
   );
     
@@ -19,15 +21,15 @@ module PBKDF2_chunk(
   
     prf_i = count == 0 ? pass_i : prf_reg // on the first iterations, input password
 
+
     // instantiate HMAC
     //HMAC_SHA256 prf (.clk_i, .rst_i, .prf_i, .salt_i, prf_o, .prf_v_o, .prf_r_i);.
 
     
-    
     always @(posedge clk_i) begin
 	if (rst_i) begin
 	    count <= 0;
-            hash_i <= 0;
+            prf_reg <= 0;
 	end else
 	    if (prf_v_o) begin // store output data, increment count and compute U_x
 		prf_r_i <= 1;	
