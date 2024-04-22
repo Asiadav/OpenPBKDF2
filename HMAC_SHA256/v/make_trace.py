afa8168114f61608119e37987d1a474928b22bed
Eli Orona <eliorona@linux-lab-057.ece.uw.edu>
2024-04-21 23:29:31 -0700

    Start looking into hmac traces

--- HMAC_SHA256/v/make_trace.py
+++ HMAC_SHA256/v/make_trace.py
@@ -1,71 +1,45 @@
 #!/bin/python3
 
 import hashlib
+import hmac
 
-def make_trace_for(input: str) -> [str]:
-    if (len(input) > 55):
-        print("Too complex to pad")
-        return []
+bytes_36 = bytes((x ^ 0x36) for x in range(256))
+bytes_5C = bytes((x ^ 0x5C) for x in range(256))
+
+def make_trace_for(key: str, message: str) -> [str]:
+    # Known working hmac
+    correct = hmac.new(key.encode(), message.encode(), hashlib.sha256).hexdigest()
+
+    # hmac reimpl to verify information
+    key_bytes = key.encode()
+    message_bytes = message.encode()
+
+    if (len(key) < 64):
+        key_bytes += b"\x00" * (64 - len(key_bytes))
+
+    i_key_pad = key_bytes.translate(bytes_36)
+    print(f"# i_key_pad: {i_key_pad.hex()}")
+
+    o_key_pad = key_bytes.translate(bytes_5C)
+    print(f"# o_key_pad: {o_key_pad.hex()}")
+
+    hash_sum_1 = hashlib.sha256(i_key_pad + message_bytes)
+
+    print(f"# hash_sum_1: {hash_sum_1.digest().hex()}")
+
+    hash_sum_2 = hashlib.sha256(o_key_pad + hash_sum_1.digest())
+
+    print(hash_sum_2.hexdigest())
+    assert hash_sum_2.hexdigest() == correct
+    # end reimpl
 
     lines: [str] = []
-
-    sha256 = hashlib.new("sha256")
-
-    reset_line = "0010__1"
-    unreset_line = "0010__0"
-    for i in range(0, 64):
-        reset_line += "_00000000"
-        unreset_line += "_00000000"
-
-    lines.append(f"# Reset SHA256")
-    lines.append(reset_line)
-    lines.append(unreset_line)
-    lines.append("")
-
-    send_line: str = "0001__0_"
-    chunk_bytes: bytes = input.encode()
-
-    for i in range(0, len(input)):
-        send_line += format(chunk_bytes[i], "08b") + "_"
-    
-    send_line += "10000000_"
-    
-    for _ in range(len(input) + 1, 56):
-        send_line += "00000000_"
-
-    for _ in range(6):
-        send_line += "00000000_"
-
-    length: str = format(len(input) * 8, "09b")
-
-    send_line += f"0000000{length[0]}_{length[1:]}"
-
-    lines.append(f"# Send `{input}`")
-    lines.append(send_line)
-
-    
-    sha256.update(input.encode())
-    digest = sha256.hexdigest()
-
-    read_line = "0010__0"
-    for i in range(0, 32):
-        read_line += "_00000000"
-
-    for i in range(0, 64, 2):
-        byte = digest[i:i+2]
-        read_line += "_" + format(int(byte, 16), "08b")
-
-    lines.append("")
-    lines.append(f"# Receive `{digest}`")
-    lines.append(read_line)
-    lines.append("\n")
-
     return lines
 
 
 if __name__ == "__main__":
     lines: [str] = []
-    lines += make_trace_for("")
-    lines += make_trace_for("123456789012345678901234567890123456780123456789012345")
+    lines += make_trace_for("key", "The quick brown fox jumps over the lazy dog")
+    # lines += make_trace_for("123456789012345678901234567890123456780123456789012345")
 
     print("\n".join(lines))
