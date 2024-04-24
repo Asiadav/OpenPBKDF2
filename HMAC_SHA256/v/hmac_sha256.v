102618ae9bde6c0a1bdce008a418303fed77f3ab
Davis Henry Sauer <dsauer@linux-lab-049.ece.uw.edu>
2024-04-24 18:05:17 +0000

    updated hmac for larger input and correct key and msg

--- HMAC_SHA256/v/hmac_sha256.v
+++ HMAC_SHA256/v/hmac_sha256.v
@@ -1,13 +1,15 @@
 /**
 * HMAC using SHA256 Encryption
+* input the password or the previously 
 **/
 
 module hmac_sha256 (
      input logic clk_i
     ,input logic rst_i 
         
-    ,input logic [255:0] prf_i
-    ,input logic [255:0] salt_i
+    ,input logic [439:0] key_i   // left aligned such that the key has all 0's on the right to fill the input
+    ,input logic [511:0] msg_i   // left aligned such that the msg has all 0's on the right to fill the input
+    ,input logic [4:0] msg_len_i
     
     ,output logic [255:0] prf_o
     
@@ -20,12 +22,13 @@ module hmac_sha256 (
     logic new_hash, in_valid, in_ready, out_valid, out_ready, sel;
     logic [2:0] ps, ns;
     logic [255:0] pad, out;
-    logic [511:0] in;
+    logic [1023:0] in;
 
-    assign pad = ps == 1 ? {16{16'h5c}} : {16{16'h36}}; // select the padding
+    assign pad = ps == 1 ? {32{16'h5c}} : {32{16'h36}}; // select the padding
 
     assign new_hash = 1;
-    sha256 hasher (.clk_i, .rst_i, .in_valid, .new_hash, .in, .in_ready, .out_valid, .out, .out_ready);
+    // TODO: update with new 1024 bit input hashing module
+    //sha256_auto_padder_1024_bit_in hasher (.clk_i, .rst_i, .in_valid, .in_length({1,msg_len_i}) .new_hash, .in, .in_ready, .out_valid, .out, .out_ready);
 
     always @(*) begin
     ns = ps; r_o = 0; out_ready = 0; in_valid = 0; v_o = 0;
@@ -64,8 +67,8 @@ module hmac_sha256 (
             prf_o <= 0;
         end else begin
             ps <= ns;
-            if (ps == 1) in <= {pad ^ salt_i, prf_i}; // update the sha256 input register
-            if (ps == 3) in <= {pad ^ salt_i, out, 8'b10000000, 56'b0, 64'd96}; // update the sha256 input register
+            if (ps == 1) in <= {pad ^ key_i, msg_i}; // update the sha256 input register
+            if (ps == 3) in <= {pad ^ key_i, out}; // update the sha256 input register
             if (ps == 5) prf_o <= out; // update the output register
         end
     end
